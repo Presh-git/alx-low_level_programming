90486a1ade9ee920e2424d78547a2be2c3a08992
Presh-git <omotoshoprecious99@gmail.com>
2022-09-08 14:25:21 -0700

    C Programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints using put
+ * Return: 0 if the program executes successfully
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
